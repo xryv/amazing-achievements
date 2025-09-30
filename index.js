774d37979080ed9c946e4305a3f87f8d453569b1
xryv <bru.cerqueira5@gmail.com>
2025-09-30 12:27:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// // Tiny module with one function to "fix"
-// export function add(a, b) {
-//   // Initial bug on purpose: returns wrong result to fail CI once
-//   // Change `a + b - 1` ➜ `a + b` in your PR to make tests pass.
-//   return a + b - 1;
-// }
+ Tiny module with one function to "fix"
+ export function add(a, b) {
+    Change `a + b - 1` ➜ `a + b` in your PR to make tests pass.
+   return a + b;
 
-export function add(a, b) {
-  return a + b; // ✅ fixed
-}
+ }
+
+
